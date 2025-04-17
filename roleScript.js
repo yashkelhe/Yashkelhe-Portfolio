4b17823c3dbdee79df43b486e4967be35fc0fff0
yash kelhe <yashkelhe30@gmail.com>
2025-04-17 09:58:05 +0530

    changes

--- roleScript.js
+++ roleScript.js
@@ -1,14 +1,16 @@
-var typeData = new Typed(".role", {
-  strings: [
-    "a Full Stack Developer",
-    "a Web Developer",
-    "a UI-UX Designer",
-    "a Backend Developer",
-    "a Coder",
-    "Yash Kelhe",
-  ],
-  loop: true,
-  typeSpeed: 100,
-  backSpeed: 80,
-  backDelay: 1000,
+document.addEventListener("DOMContentLoaded", function () {
+  var typeData = new Typed(".role", {
+    strings: [
+      "a Full Stack Developer",
+      "a Web Developer",
+      "a UI-UX Designer",
+      "a Backend Developer",
+      "a Coder",
+      "Yash Kelhe",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
 });
